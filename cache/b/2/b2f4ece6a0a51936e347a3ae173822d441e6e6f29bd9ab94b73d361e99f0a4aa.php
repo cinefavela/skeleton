453275f54d97642b6979baa68e65cac2f355fbbd
ricardo <ricardo@debian>
2015-08-13 00:05:14 -0300

    Initial commit

---
+++ cache/b/2/b2f4ece6a0a51936e347a3ae173822d441e6e6f29bd9ab94b73d361e99f0a4aa.php
@@ -0,0 +1,31 @@
+<?php
+
+/* index.html */
+class __TwigTemplate_b2f4ece6a0a51936e347a3ae173822d441e6e6f29bd9ab94b73d361e99f0a4aa extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "teste";
+    }
+
+    public function getTemplateName()
+    {
+        return "index.html";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
